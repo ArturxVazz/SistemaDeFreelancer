1e4df035ac7b5b6253bbe93d7dcc59f7c43fc02d
Artur Silva Vaz <162496351+ArturxVazz@users.noreply.github.com>
2025-05-06 16:14:20 -0300

    Adicionando mais metodos

--- systemOfFreelancers/src/Service/FreelancerService.java
+++ systemOfFreelancers/src/Service/FreelancerService.java
@@ -1,15 +1,61 @@
 package Service;
 
 import Entities.FreelancerEntity;
+import Entities.ProjetoEntity;
 import Entities.SistemaFreeLancersEntity;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class FreelancerService {
-    SistemaFreeLancersEntity n1 = new SistemaFreeLancersEntity();
+    SistemaFreeLancersEntity s1 = new SistemaFreeLancersEntity();
 
-    public void cadastroUsuario(FreelancerEntity freelancer) {
-        n1.getListaFreelancers().add(freelancer);
+    public void adicionarProjeto(FreelancerEntity freelancer, ProjetoEntity projeto) {
+        freelancer.getListaProjetos().add(projeto);
     }
+
+    public FreelancerEntity criarFreelancer(){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite o Nome do freeLancer:");
+        String nomeFreeLancer = input.nextLine();
+
+        System.out.println("Digite a area do freeLancer:");
+        String areaAtuacao = input.nextLine();
+
+        FreelancerEntity f1 = new FreelancerEntity(nomeFreeLancer, areaAtuacao);
+
+        System.out.println("Usuario Cadastrado com sucesso");
+
+        return f1;
+    }
+
+    public String mostrandoGanhos (FreelancerEntity f1){
+        return ("Seu ganhos totais foram: " + calculoGanhosTotais());
+    }
+
+    public static double calculoGanhosTotais()
+    {
+        double variavelDeControle = 0.0;
+
+        for(ProjetoEntity projeto : s1.getListaFreelancers() )
+        {
+            variavelDeControle += projeto.getValor();
+        }
+
+        return variavelDeControle;
+
+    }
+
+
+
+
+
+
+
+
+
+
 }
--- systemOfFreelancers/src/Service/SistemaFreeLancersService.java
+++ systemOfFreelancers/src/Service/SistemaFreeLancersService.java
@@ -1,5 +1,32 @@
 package Service;
 
+import Entities.FreelancerEntity;
+import Entities.ProjetoEntity;
+import Entities.SistemaFreeLancersEntity;
+
 public class SistemaFreeLancersService {
+    SistemaFreeLancersEntity n1 = new SistemaFreeLancersEntity();
+    FreelancerService f1 = new FreelancerService();
+
+    public void cadastroUsuario(FreelancerEntity freelancer) {
+        n1.getListaFreelancers().add(freelancer);
+    }
+
+    public void adicionarProjetoFreelancer(FreelancerEntity freelancer, ProjetoEntity projeto) {
+        f1.adicionarProjeto(freelancer, projeto);
+    }
+
+    public void mostrarFreeLancers() {
+        int i = 1;
+        for (FreelancerEntity freelancer : n1.getListaFreelancers()) {
+            System.out.println("Freelancer " + i++ + ": " + freelancer.getNome());
+        }
+    }
+
+
+
+
+
+
 
 }
